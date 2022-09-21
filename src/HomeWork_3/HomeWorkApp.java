b22dd878dfef7e1ccc3939e5500f681b254b6cc3
Igor-Liholit <igorliholit9797@gmail.com>
2022-09-21 17:34:19 +0300

    did all but 10

--- src/HomeWork_3/HomeWorkApp.java
+++ src/HomeWork_3/HomeWorkApp.java
@@ -10,50 +10,83 @@ public class HomeWorkApp {
         printColor();
         System.out.println();
         compareNumbers();
+        System.out.println();
+        System.out.println(testBoolean(15, 10));
+        System.out.println();
+        definitionNumber(-7);
+        System.out.println();
+        System.out.println(chekBoolean(-5));
+        System.out.println();
+        stringAndNumbers("Hello", 4);
+        System.out.println(leapYear(1900));
     }
 
-    public static void  printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+    public static void printThreeWords() {
+        System.out.println("Orange \nBanana \nApple");
     }
 
-    public static void checkSumSign(){
+    public static void checkSumSign() {
         int a = 58;
         int b = 78;
         int sum = a + b;
-
-        if (sum >=0){
+        if (sum >= 0) {
             System.out.println("Сумма положительная");
-        }
-        else {
+        } else {
             System.out.println("Сумма отрицательная");
         }
     }
 
-
-    public static void printColor(){
+    public static void printColor() {
         int value = 100;
-        if (value <= 0){
+        if (value <= 0) {
             System.out.println("Красный");
-        } else if  (value > 0 & value <=99) {
+        } else if (value > 0 & value <= 99) {
             System.out.println("Желтый");
-        }
-        else{
+        } else {
             System.out.println("Зеленый");
         }
     }
 
-    public  static void compareNumbers(){
+    public static void compareNumbers() {
         int a = 87;
         int b = 100;
-        if (a >=b){
+        if (a >= b) {
             System.out.println("a >= b");
-        }
-        else{
-            System.out.println( "a < b");
+        } else {
+            System.out.println("a < b");
         }
     }
 
+    public static boolean testBoolean(int a, int b) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
+            return true;
+        }
+        return false;
+    }
 
+    public static void definitionNumber(int q) {
+        if (q >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    public static boolean chekBoolean(int e) {
+        if (e < 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void stringAndNumbers(String str, int g) {
+        for (int i = 1; i <= g; i++) {
+            System.out.println(i + " " + str);
+        }
+    }
+
+    public static boolean leapYear(int year) {
+
+    }
 }
