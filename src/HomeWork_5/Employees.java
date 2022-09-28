3b875e2688a0563acacd70efa38ae7cf35c2c18f
Игорь <igorliholit9797@gmail.com>
2022-09-28 22:06:24 +0300

    finished encapsulation

---
+++ src/HomeWork_5/Employees.java
@@ -0,0 +1,49 @@
+package HomeWork_5;
+
+public class Employees {
+        private String fullName;
+        private String jobTitle;
+        private String email;
+        private int employeePhone;
+        private int employeesAge;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEmployeePhone() {
+        return employeePhone;
+    }
+
+    public void setEmployeePhone(int employeePhone) {
+        this.employeePhone = employeePhone;
+    }
+
+    public int getEmployeesAge() {
+        return employeesAge;
+    }
+
+    public void setEmployeesAge(int employeesAge) {
+        this.employeesAge = employeesAge;
+    }
+}
---
+++ src/HomeWork_5/EmployeesFromWork.java
@@ -0,0 +1,12 @@
+package HomeWork_5;
+
+public class EmployeesFromWork {
+    public static void main(String[] args) {
+        Employees employees = new Employees ();
+        employees.setFullName( "Ivan");
+        employees.setJobTitle("HR");
+        employees.setEmail("ivan225@gmail.com");
+        employees.setEmployeePhone(+38554445);
+        employees.setEmployeesAge(28);
+    }
+}
