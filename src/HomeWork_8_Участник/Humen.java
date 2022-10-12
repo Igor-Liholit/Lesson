7b34c6adb82b5a4334c597429d177ed242c52c39
Игорь <igorliholit9797@gmail.com>
2022-10-12 16:25:24 +0300

    Finished HW

--- src/HomeWork_8_Участник/Humen.java
+++
@@ -1,8 +0,0 @@
-package HomeWork_8_Участник;
-
-public class Humen extends Participant {
-
-    public Humen(String view, double jumpLong, double runDictance) {
-        super( view , jumpLong, runDictance);
-    }
-}
--- src/HomeWork_8_Участник/Robot.java
+++
@@ -1,8 +0,0 @@
-package HomeWork_8_Участник;
-
-public class Robot  extends Participant{
-
-    public Robot( String view, double jumpLong, double runDictance) {
-        super( view , jumpLong, runDictance);
-    }
-}
